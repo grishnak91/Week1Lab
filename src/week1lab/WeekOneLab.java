2e4dcb6079d736552e6545816543de7dde8e96b3
Jordan Staires <grishnak91@icloud.com>
2022-09-07 12:18:07 -0500

    first commit of actual code

---
+++ src/week1lab/WeekOneLab.java
@@ -0,0 +1,24 @@
+package week1lab;
+
+public class WeekOneLab {
+
+	public static void main(String[] args) {
+		
+		double moneyToSpend = 60.75;
+		char middleInitial = 'P';
+		boolean isHotOutside = false;
+		String myFirstName = "MC";
+		String streetAddress = "612 Wharf Avenue";
+		
+		System.out.println("I'm to spend " + moneyToSpend + " on freaking cheese.");
+		System.out.println("Customer's middle initial is " + middleInitial);
+		System.out.println("It is hot outside- " + isHotOutside);
+		System.out.println("My first name is " + myFirstName);
+		System.out.println("Address of my domicile is " + streetAddress);
+		
+		
+		
+
+	}
+
+}
